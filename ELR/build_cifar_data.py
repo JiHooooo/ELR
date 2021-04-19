6d0492837c2e168a1b1a784cbcee202091d0352d
Jh.Hu <jhhuku@gmail.com>
2021-04-19 18:04:57 +0900

    modify cifar dataset loading tool

--- ELR/build_cifar_data.py
+++ ELR/build_cifar_data.py
@@ -11,12 +11,13 @@ def save_image(np_array, index_list, save_folder, label_name,ext='bmp'):
 save_folder = 'data'
 sub_folder = ['train', 'val']
 
-dataset = torchvision.datasets.CIFAR10(dataset_folder, train=True, download=True)
-dataset_val = torchvision.datasets.CIFAR10(dataset_folder, train=False, download=True)
+dataset = torchvision.datasets.CIFAR10(save_folder, train=True, download=True)
+dataset_val = torchvision.datasets.CIFAR10(save_folder, train=False, download=True)
 
 label_name_dist = {j:i for i,j in dataset.class_to_idx.items()}
 for dataset_one, sub_folder_one in zip([dataset, dataset_val], sub_folder):
     for label_index, label_name in label_name_dist.items():
         save_folder_one = '%s/%s/%s'%(save_folder, sub_folder, label_name)
+        os.makedirs(save_folder_one)
         one_type_data = dataset_one.data[np.where(np.array(dataset_one.targets) == label_index)]
-        save_image(one_type_data, [i for range(len(one_type_data))], save_folder_one, label_name)
+        save_image(one_type_data, [i for i in range(len(one_type_data))], save_folder_one, label_name)
